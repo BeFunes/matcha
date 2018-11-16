bfade9b3b60a766bfac372da8850a38fd71e98b1
Benedetta DAL CANTON <bdal-can@student.42.fr>
2018-11-16 16:15:04 +0100

    user

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const parser = require('body-parser')
 const app = express()
 
 const adminRoutes = require('./routes/admin')
+const userRoutes = require('./routes/user')
 
 app.use(parser.urlencoded({extended: false}))
 
 app.use('/admin', adminRoutes)
+app.use('/', userRoutes)
 
 app.listen(3000)
 
