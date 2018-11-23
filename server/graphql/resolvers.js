fc3e0d322d2d3de4879fecd414b08e53e0ea0726
Raphael Ghirelli <rghirell@student.42.fr>
2018-11-23 16:44:14 +0100

    beginning of great adventure
    Signed-off-by: Raphael Ghirelli <rghirell@student.42.fr>

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,11 +1,16 @@
-const db = require('../util/database');
+const db = require('../util/db');
 const bcrypt = require('bcryptjs');
 
 
 module.exports = {
-    createUser: async function({ userInput}, req) {
-        const [row, column] = await db.execute('SELECT * FROM users WHERE email=?', userInput.email)
-        console.log(row, column)
-        return {id: '1', email: 'someemail@you.com', password: 'pw'}
+    createUser: async function({ userInput }, req) {
+        const [row] = await db.query('SELECT * FROM users WHERE email=?', userInput.email)
+        if (row.length > 0) {
+            throw new Error('NOt happy')
+        }
+        const hashedPw = await bcrypt.hash(userInput.password, 12);
+        await db.query('Insert into users (email, password) VALUES (?, ?)', [userInput.email, hashedPw])
+        console.log(row)
+        return {id: '1', email: userInput.email, password: userInput.password}
     }
 };
--- server/util/db.js
+++ server/util/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'matchadb',
+    database: 'matcha',
     password: 'Apple123'
 });
 
