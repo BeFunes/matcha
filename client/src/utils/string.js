540a8ccfa89a5bf9d0f682302145efab89508ca3
BeFunes <benedetta.dalcanton@gmail.com>
2018-11-27 15:13:15 +0100

    little fix

--- client/src/utils/string.js
+++ client/src/utils/string.js
@@ -3,10 +3,10 @@ const validatorAux = (value, type) => {
 	switch (type) {
 		case 'email':
 			const emailPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-			return unicodePattern.test(value) && emailPattern.test(value)
+			return !unicodePattern.test(value) && emailPattern.test(value)
 		case 'password':
 			const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/   /// must contain one lowercase, one uppercase, one digit. To add a symbol: (?=.*[!@#\$%\^&\*])
-			return unicodePattern.test(value) && passwordPattern.test(value)
+			return !unicodePattern.test(value) && passwordPattern.test(value)
 		default:
 			return true
 	}
