74b23ed059c7df31c46a2f9491c7bda033a8f0c1
BeFunes <benedetta.dalcanton@gmail.com>
2018-11-29 15:30:06 +0100

    faker!

--- faker.js
+++ faker.js
@@ -1,17 +1,46 @@
-// const faker = require(faker);
+const faker = require('faker/locale/fr');
 const mysql = require('mysql2');
+const datesBetween = require('dates-between')
+const bcrypt = require('bcryptjs');
 
 const db = mysql.createConnection({
  host: "localhost",
  user: "root",
  password: "Apple123",
- database: "matcha"
+ database: "matchadb"
 });
 
-db.connect(function(err) {
- if (err) throw err;
- console.log("Connected!");
- const query = `CREATE TABLE users (
+const getOrientation = (gender, orientation) => {
+	const oppositeSex = gender === 'M' ? 'F' : 'M'
+	switch (orientation) {
+		case "straight" :
+			return oppositeSex
+		case "gay" :
+			return gender
+		default:
+			return "B"
+	}
+}
+
+
+const dummyPassword = "$2a$12$rZHGfYxrMBjazgmd.OXq3OiH5wiocqYo6QB5Mxp6I2msv/JnGQL2K"
+
+const getData = async function (gender, orient) {
+	const genderCode = gender === 'M' ? 0 : 1
+	const firstName = faker.name.firstName(genderCode);
+	const lastName = faker.name.lastName(genderCode); // Kassandra.Haley@erich.biz
+	const email = `${firstName}.${lastName}@hotmail.com`
+	const password = dummyPassword
+	const birthDate = '1970-01-01';
+	const orientation = getOrientation(gender, orient)
+	const occupation = faker.name.jobTitle() //
+	const bio = faker.lorem.paragraph()
+	const profilePicture = faker.image.avatar()
+	return [firstName, lastName, email, password, birthDate, gender, orientation, occupation, bio, profilePicture]
+}
+
+
+const createTableQuery = `CREATE TABLE users (
     id int(11) unsigned NOT NULL AUTO_INCREMENT,
     first_name varchar(20),
     last_name varchar(30),
@@ -30,32 +59,42 @@ db.connect(function(err) {
     isOnboarded tinyint(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4`;
- db.query(query, function (err, result) {
-   if (err) throw err;
-   console.log("Table created");
- });
- db.end()
+
+const populateTableQuery = `INSERT INTO users (first_name, last_name, email, password,
+birthDate, gender, orientation, occupation, bio, profilePic, 
+picture2, picture3, picture4, picture5, isOnboarded)
+VALUES
+(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NULL, NULL, NULL, NULL, 1)`
+// const data = [firstName, lastName, email, password, birthDate, gender, orientation, occupation, bio, profilePicture];
+
+
+const getRandomUser = () => {
+	const sex = ['M', 'F']
+	const orient = ['straight', 'gay', 'bisexual']
+	return { gender : sex[Math.floor(Math.random()*sex.length)], orientation : orient[Math.floor(Math.random()*orient.length)]}
+}
+
+
+db.connect(async function (err) {
+ if (err) throw err;
+ console.log("Connected!");
+	db.query("DROP TABLE IF EXISTS users", function (err, result) {
+		if (err) throw err;
+		console.log("Table deleted");
+	});
+	db.query(createTableQuery, function (err, result) {
+		if (err) throw err;
+		console.log("Table users created");
+	});
+	for (let i = 0; i < 300; i++) {
+		let user = getRandomUser()
+		const data = await getData(user.gender, user.orientation)
+		db.query(populateTableQuery, data,  function (err, result) {
+			if (err) throw err;
+		});
+	}
+	console.log("Data Inserted");
+
+	db.end()
 });
-
-
-
-
-
-// const db = pool.promise()
-// db.query(CREATE TABLE users (
-//     id int(9) unsigned NOT NULL AUTO_INCREMENT,
-//     first_name varchar(20) NOT NULL,
-//     last_name varchar(100) NOT NULL,
-//     email varchar(40) NOT NULL,
-//     password varchar(60) NOT NULL,
-//     gender varchar(10) NOT NULL,
-//     sexual_preference varchar(20) NOT NULL,
-//     bio text NOT NULL,
-//     interests set(item1,item2,item3) NOT NULL,
-//     pictures set(item1,item2,item3) NOT NULL,
-//     country varchar(40) NOT NULL,
-//     city varchar(20) NOT NULL,
-//     UNIQUE KEY email (email),
-//     KEY id (id)
-//    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;)
 
