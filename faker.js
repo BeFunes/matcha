cee82f9cc368039f558a7ab6338bd538ca6ea62d
Raphael Ghirelli <rghirell@student.42.fr>
2018-11-29 13:39:22 +0100

    faker create db
    Signed-off-by: Raphael Ghirelli <rghirell@student.42.fr>

--- faker.js
+++ faker.js
@@ -1,20 +1,61 @@
-// const faker = require('faker');
+// const faker = require(faker);
+const mysql = require('mysql2');
 
-const faker = require('faker/locale/fr');
-faker.seed(123);
+const db = mysql.createConnection({
+ host: "localhost",
+ user: "root",
+ password: "Apple123",
+ database: "matcha"
+});
+
+db.connect(function(err) {
+ if (err) throw err;
+ console.log("Connected!");
+ const query = `CREATE TABLE users (
+    id int(11) unsigned NOT NULL AUTO_INCREMENT,
+    first_name varchar(20),
+    last_name varchar(30),
+    email varchar(70)  NOT NULL,
+    password varchar(70)  NOT NULL,
+    birthDate date DEFAULT NULL,
+    gender char(1),
+    orientation char(1),
+    occupation varchar(50),
+    bio varchar(3000),
+    profilePic varchar(255),
+    picture2 varchar(255),
+    picture3 varchar(255),
+    picture4 varchar(255),
+    picture5 varchar(255),
+    isOnboarded tinyint(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4`;
+ db.query(query, function (err, result) {
+   if (err) throw err;
+   console.log("Table created");
+ });
+ db.end()
+});
 
 
-const randomName = faker.name.findName(); // Rowan Nikolaus
-const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-// const randomCard = faker.helpers.createCard(); // random contact card containing many properties
-const usercard = faker.address()
 
 
-console.log(randomName)
-console.log(randomEmail)
-// console.log(randomCard)
-// console.log(faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}} {{internet.email}}"));
 
-// console.log(faker.fake("{{address}}"));
+// const db = pool.promise()
+// db.query(CREATE TABLE users (
+//     id int(9) unsigned NOT NULL AUTO_INCREMENT,
+//     first_name varchar(20) NOT NULL,
+//     last_name varchar(100) NOT NULL,
+//     email varchar(40) NOT NULL,
+//     password varchar(60) NOT NULL,
+//     gender varchar(10) NOT NULL,
+//     sexual_preference varchar(20) NOT NULL,
+//     bio text NOT NULL,
+//     interests set(item1,item2,item3) NOT NULL,
+//     pictures set(item1,item2,item3) NOT NULL,
+//     country varchar(40) NOT NULL,
+//     city varchar(20) NOT NULL,
+//     UNIQUE KEY email (email),
+//     KEY id (id)
+//    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;)
 
-console.log(usercard)
