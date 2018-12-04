f7a2087220f038c653b059290ac27bacbd1b5d41
BeFunes <benedetta.dalcanton@gmail.com>
2018-12-04 16:47:26 +0100

    App.js cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -35,60 +35,6 @@ class App extends Component {
 		this.setAutoLogout(remainingTime)
 	}
 
-	// loginHandler = (authData) => {
-	// 	console.log("LOGIN HANDLER")  ////////////////////REMOVE
-	// 	const query = {
-	// 		query: `{
-   //              login(email: "${authData.email}", password: "${authData.password}") {
-   //                  token
-   //                  userId
-   //                  isOnboarded
-   //              }
-   //          } `
-	// 	}
-	// 	fetch('http://localhost:3001/graphql', {
-	// 		method: 'POST',
-	// 		headers: {
-	// 			'Content-Type': 'application/json'
-	// 		},
-	// 		body: JSON.stringify(query)
-	// 	})
-	// 		.then(res => {
-	// 			return res.json()
-	// 		})
-	// 		.then(resData => {
-	// 			if (resData.errors && resData.errors[0].status === 422) {
-	// 				throw new Error(
-	// 					"Validation failed."
-	// 				)
-	// 			}
-	// 			if (resData.errors) {
-	// 				this.setState({isAuth: false, loginFail: true})
-	// 				throw new Error ("User login failed.")
-	// 			}
-	// 			console.log(resData)
-	//
-	// 			this.setState({
-	// 				isAuth: true,
-	// 				token: resData.data.login.token,
-	// 				userId: resData.data.login.userId,
-	// 				isOnboarded: resData.data.login.isOnboarded,
-	// 			})
-	// 			const expiryDate = new Date (new Date().getTime() + 60*60*1000)
-	// 			localStorage.setItem('token', resData.data.login.token)
-	// 			localStorage.setItem('userId', resData.data.login.userId)
-	// 			localStorage.setItem('expiryDate', expiryDate.toISOString())
-	// 			localStorage.setItem('isOnboarded', resData.data.login.isOnboarded)
-	// 			this.setAutoLogout(60*60*1000)
-	// 		})
-	// 		.catch(err => {
-	// 			console.log(err)
-	// 			this.setState({
-	// 				isAuth: false,
-	// 			})
-	// 		})
-	// }
-
 	loginHandler = (data) => {
 		this.setState({
 			isAuth: true,
@@ -109,6 +55,7 @@ class App extends Component {
 		localStorage.removeItem('token');
 		localStorage.removeItem('expiryDate');
 		localStorage.removeItem('userId');
+		localStorage.removeItem('isOnboarded');
 	};
 
 	setAutoLogout = milliseconds => {
@@ -123,18 +70,6 @@ class App extends Component {
 	}
 
 	render() {
-		const main = (
-			<div>
-				{this.state.isAuth && this.state.isOnboarded && <Toolbar />}
-				<Switch> {/* with switch, the route will consider only the first match rather than cascading down!*/}
-					<Route path="/" exact component={Browse}/>
-					<Route path="/profile" component={Profile}/>
-					<Route path="/chat" component={Chat}/>
-					<Route path="/onboarding" component={Onboarding}/>
-				</Switch>
-			</div>
-			)
-
 
 		const routeZero = () => {
 			if (this.state.isAuth && this.state.isOnboarded)
