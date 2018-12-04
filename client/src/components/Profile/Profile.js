4009479657ee3d6340a25bfc08b8d09b7cd6b97a
BeFunes <benedetta.dalcanton@gmail.com>
2018-12-04 16:46:03 +0100

    playing around with profile page

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
-// import styles from './Profile.module.css'
+import styles from './Profile.module.css'
+import TextInput from "../UI/TextInput/TextInput";
+
+// state = {
+// 	textInput: {
+// 		firstName: {
+// 			value: '',
+// 		}
+//
+// 	}
+// }
+
 
 class Profile extends Component {
 	render() {
 		return (
-			<div>
-				 YOUR PROFILE
+			<div className={styles.component}>
+				<div className={styles.names}>
+					<TextInput/>
+				</div>
 			</div>
 		)
 	}
