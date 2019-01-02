bd37b6b0fa66359abd574582030f8d394c27f387
BeFunes <benedetta.dalcanton@gmail.com>
2019-01-02 13:40:19 +0100

    geolocation Dialog style

--- client/src/components/GeolocationDialog/GeolocationDialog.js
+++ client/src/components/GeolocationDialog/GeolocationDialog.js
@@ -1,17 +1,21 @@
 import React, {Component} from 'react'
 import Dialog from "@material-ui/core/es/Dialog/Dialog";
 import Button from "@material-ui/core/es/Button/Button";
-
+import styles from './GeolocationDialog.module.css'
 class GeolocationDialog extends Component {
 
 	render() {
 		const {onClose, open, onYes, location} = this.props
-		console.log(location)
 		return (
 			<Dialog onClose={onClose} open={open}>
-				Update your location to {location.address}?
-				<Button onClick={onClose}>NO</Button>
-				<Button onClick={onYes}>YES</Button>
+				<div className={styles.text}>
+				<div>Update your location to </div>
+					<div className={styles.location}>{location.address}?</div>
+				</div>
+					<div className={styles.buttons}>
+				<Button variant='contained' onClick={onClose}>NO</Button>
+				<Button variant='contained' color='secondary' onClick={onYes}>YES</Button>
+				</div>
 			</Dialog>
 		);
 	}
