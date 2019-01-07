c23e3894134edb3792ddba9e23c043a58daa72cb
BeFunes <benedetta.dalcanton@gmail.com>
2019-01-07 16:06:09 +0100

    you have no notifications yet

--- client/src/components/NotificationsDrawer/Notifications/Notifications.js
+++ client/src/components/NotificationsDrawer/Notifications/Notifications.js
@@ -7,8 +7,8 @@ import List from "@material-ui/core/es/List/List";
 import withStyles from "@material-ui/core/es/styles/withStyles";
 
 const listTextStyle = () => ({
-	listItemText:{
-		fontSize:'15px',//Insert your required size
+	listItemText: {
+		fontSize: '15px',//Insert your required size
 	},
 	secondaryText: {
 		fontSize: '12px',
@@ -29,9 +29,9 @@ class Notifications extends Component {
 		const {notifications} = this.props
 		const cellBackground = (x) => !!x.seen ? '#ECECEC' : '#ffe4e8'
 		const text = (name) => ({
-			"match" : <div>You matched with <h4>{name}</h4></div>,
-			"like" : <div><h4>{name}</h4> liked you</div>,
-			"unmatch" : <div>You unmatched with <h4>{name}</h4></div>,
+			"match": <div>You matched with <h4>{name}</h4></div>,
+			"like": <div><h4>{name}</h4> liked you</div>,
+			"unmatch": <div>You unmatched with <h4>{name}</h4></div>,
 			"visited": <div><h4>{name}</h4> visited your profile</div>
 		})
 
@@ -39,34 +39,38 @@ class Notifications extends Component {
 			<div className={styles.component}>
 				<div className={styles.header}>
 					<div className={styles.closeIcon} onClick={this.closeAndMarkAllAsSeen}>
-							<CloseIcon style={{fontSize: 30}}/>
+						<CloseIcon style={{fontSize: 30}}/>
 					</div>
 					Notifications
-				</div >
-				{notifications && <List className={styles.list} >
-					{notifications.map((x, i) => (
-						<ListItem button key={i}
-						          style={{borderBottom: "1px solid white", backgroundColor: cellBackground(x)}}
-						          onClick={() => {
-							          this.props.history.push({
-								          pathname: `/user_profile`,
-								          search: '',
-								          state: { id: parseInt(x.senderId)}
-							          })
-						          }}
-						>
-							<ListItemText
-								classes={{primary: this.props.classes.listItemText,
-													secondary: this.props.classes.secondaryText}}
-								primary={text(x.senderName)[x.type]}
-								secondary={x.createdAt}
-							/>
-						</ListItem>
-					))}
-				</List>}
+				</div>
+				{notifications && notifications.length > 0 ? <List className={styles.list}>
+						{notifications.map((x, i) => (
+							<ListItem button key={i}
+							          style={{borderBottom: "1px solid white", backgroundColor: cellBackground(x)}}
+							          onClick={() => {
+								          this.props.history.push({
+									          pathname: `/user_profile`,
+									          search: '',
+									          state: {id: parseInt(x.senderId)}
+								          })
+							          }}
+							>
+								<ListItemText
+									classes={{
+										primary: this.props.classes.listItemText,
+										secondary: this.props.classes.secondaryText
+									}}
+									primary={text(x.senderName)[x.type]}
+									secondary={x.createdAt}
+								/>
+							</ListItem>
+						))}
+					</List> :
+					<div className={styles.noNotifications}>You have no notifications yet</div>
+				}
 			</div>
 		)
 	}
 }
 
-export default withStyles(listTextStyle, { withTheme: true })(Notifications);
+export default withStyles(listTextStyle, {withTheme: true})(Notifications);
