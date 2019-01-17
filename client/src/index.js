db8c0905b137153816a8d88ca669fd42cf9c3d58
BeFunes <benedetta.dalcanton@gmail.com>
2019-01-17 13:19:57 +0100

    middleware on socket

--- client/src/index.js
+++ client/src/index.js
@@ -6,24 +6,37 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import theme from './theme';
-
-import { WebSocketLink } from 'apollo-link-ws'
-import { SubscriptionClient } from "subscriptions-transport-ws";
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
+import {WebSocketLink} from 'apollo-link-ws'
+import {SubscriptionClient} from "subscriptions-transport-ws";
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
 import {HOST} from "./constants";
-
+import { ApolloLink , concat} from 'apollo-link';
 
 const GRAPHQL_ENDPOINT = `ws://${HOST}:3002/graphql`;
 
-const link = new WebSocketLink(
-	new SubscriptionClient(GRAPHQL_ENDPOINT, {
-	reconnect: true
-}))
+const wsClient = new SubscriptionClient(GRAPHQL_ENDPOINT, {
+	reconnect: true,
+	connectionParams: {
+		authToken: localStorage.getItem('token')
+	}
+})
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+	const token = localStorage.getItem('token')
+	operation.setContext({
+		headers: {
+			authorization: token ? `Bearer ${token}` : null
+		}
+	})
+	return forward(operation)
+})
+
+const link = new WebSocketLink(wsClient)
 
 const client = new ApolloClient({
-	link,
+	link: concat(authMiddleware, link),
 	cache: new InMemoryCache()
 })
 
